3bd761ad5f27d6cf86a2d179eeea53582bdaf2ba
Ibrahim Hassan Shehata Sayed <146327516+IbrahimAwiby@users.noreply.github.com>
2025-09-12 15:51:49 +0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,7 @@
 import { Link } from "react-router-dom";
 import { getImageUrl } from "../../services/api";
-import { useContext } from "react";
-import { LanguageContext } from "../App";
 
 const MovieCard = ({ movie }) => {
-  const { language } = useContext(LanguageContext);
-
   return (
     <div className="bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
       <Link
@@ -38,10 +34,12 @@ const MovieCard = ({ movie }) => {
             >
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
             </svg>
-            {movie.vote_average.toFixed(1)}
+            {movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"}
           </span>
           <span className="text-gray-600 dark:text-gray-400 text-sm">
-            {new Date(movie.release_date).getFullYear()}
+            {movie.release_date
+              ? new Date(movie.release_date).getFullYear()
+              : "N/A"}
           </span>
         </div>
       </div>
